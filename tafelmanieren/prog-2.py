5d9e51882f4608bf47be7076f0db22353e6dd8ef
HamzaHammouti <hamzahhammouti@gmail.com>
2021-10-05 11:36:07 +0200

    4 tafelmanieren af

---
+++ tafelmanieren/prog-2.py
@@ -0,0 +1,3 @@
+for i in range(30,0,-1):
+    print(i)
+print("De raket wordt gelanceerd!")
---
+++ tafelmanieren/prog-3.py
@@ -0,0 +1,4 @@
+for i in range(0,12 ,1):
+    print(str(i)+ ":00" + " AM")
+for i in range(0,12 ,1):
+    print(str(i)+ ":00" + " PM")
